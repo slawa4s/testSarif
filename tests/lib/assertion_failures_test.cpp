6b5c43427d8ddddcf8264bf4b9a43577f096867f
Oleg <olezhkabobrov@gmail.com>
2022-05-13 14:33:23 +0300

    g

--- tests/lib/assertion_failures_test.cpp
+++ tests/lib/assertion_failures_test.cpp
@@ -10,7 +10,34 @@ namespace UTBot {
 static const float utbot_abs_error = 1e-6;
 
 
+#pragma region regression
+
+TEST(regression, buggy_function1_test_1)
+{
+    int actual = buggy_function1(0, 0);
+    EXPECT_EQ(0, actual);
+}
+
+TEST(regression, buggy_function1_test_2)
+{
+    int actual = buggy_function1(0, -1);
+    EXPECT_EQ(0, actual);
+}
+
+
+TEST(regression, buggy_function3_test_1)
+{
+    int actual = buggy_function3(0, 0);
+    EXPECT_EQ(0, actual);
+}
+
+#pragma endregion
 #pragma region error
+TEST(error, buggy_function1_test_3)
+{
+    buggy_function1(42, 0);
+}
+
 TEST(error, buggy_function2_test_1)
 {
     buggy_function2(7);
@@ -19,6 +46,11 @@ TEST(error, buggy_function2_test_1)
 TEST(error, buggy_function2_test_2)
 {
     buggy_function2(0);
+}
+
+TEST(error, buggy_function3_test_2)
+{
+    buggy_function3(1, 0);
 }
 
 #pragma endregion
